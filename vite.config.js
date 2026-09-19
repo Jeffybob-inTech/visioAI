9632cb28140ff429d45a50c7139e2780af7c35d1
Jeffybob-InTech <jcmercer0713@gmail.com>
2026-09-19 07:46:25 -0400

    updating config to listen to correct server + apis set up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'client',
   envDir: 'client',
-  server: { port: 5173, strictPort: true, proxy: { '/api': 'http://127.0.0.1:3001' } },
+  server: { port: 5173, strictPort: true, proxy: { '/api': 'https://visioai-xqse.onrender.com' } },
   build: { outDir: 'dist', emptyOutDir: true, target: 'es2022' },
 });
